03f5e07c2009cd42bd44568e9865bb185504a7d0
midyseprox17 <midyseprox17@gmail.com>
2019-08-18 20:29:27 +0700

    Update v_login.php

--- application/views/global/v_login.php
+++ application/views/global/v_login.php
@@ -26,28 +26,29 @@
   <div class="container">
 
     <!-- Outer Row -->
-    <div class="row justify-content-center rounded" style="">
-      <div class="col-xl-5 col-lg-5 col-md-5 rounded">
-        <div class="card o-hidden border-0 shadow-lg my-5 rounded">
-          <div class="card-body p-0 rounded">
+    <div class="row justify-content-center">
+      <div class="col-xl-5 col-lg-5 col-md-5">
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
-            <div class="row rounded">
+            <div class="row">
               <div class="col-lg-12">
-                <div class="p-5 rounded">
+                <div class="p-5">
                   <div class="text-center">
                     <img src="../img/logo.png" class="rounded mx-auto d-block mb-4" style="width: 50%">
                     <h1 class="h3 text-gray-900 mb-4">Login Aplikasi Surat Perintah</h1>
+                    <h4><?=$_SESSION['pesan']?></h4>
                     <hr class=" mb-4" style="width: 100%">
                   </div>
-                  <form class="user">
+                  <form method="post" class="user" action="<?=base_url().'login'?>">
                     <div class="form-group">
-                      <input class="form-control form-control-user rounded" name="username" id="username" aria-describedby="username" placeholder="Masukan NIP">
+                      <input class="form-control form-control-user" name="username" id="username" aria-describedby="username" placeholder="Masukan NIP">
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control rounded form-control-user" name="password" id="password" placeholder="Password">
+                      <input type="password" class="form-control form-control-user" name="password" id="password" placeholder="Password">
                     </div>
                     <div class="form-group">
-                      <button type="button" class="btn btn-primary rounded btn-user btn-block" id="submit" name="submit">Login</button>
+                      <button type="submit" class="btn btn-primary btn-user btn-block" id="submit" name="submit">Login</button>
                     </div>
                   </form>
                 </div>
