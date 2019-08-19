659ea57da96ccda56fa6e2a6160d93211e47e82a
midyseprox17 <midyseprox17@gmail.com>
2019-08-19 09:20:13 +0700

    pegawai

--- application/controllers/Pegawai.php
+++ application/controllers/Pegawai.php
@@ -5,8 +5,13 @@ class pegawai extends CI_Controller
 {
 	public function index(){
 		if($this->session->userdata('masuk') == '1'){
+			$where = array(
+				'dihapus' => '0'
+			);
+			$data['pegawai'] = $this->m_uptd->tampil_where('tbl_pegawai', $where);
+
 			$this->load->view('global/v_sidebar');
-			$this->load->view('pegawai/v_pegawai');
+			$this->load->view('pegawai/v_pegawai', $data);
 			$this->load->view('global/v_footer');
 		}else{
 			redirect(base_url().'login');
