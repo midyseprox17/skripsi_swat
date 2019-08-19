45814a4e18165e2619e2af5dc8a1b96f7cb0f37b
Rizalu Ilman <ilmanrizalu@gmail.com>
2019-08-19 11:35:07 +0700

    update lagi

--- application/views/global/v_sidebar.php
+++ application/views/global/v_sidebar.php
@@ -10,6 +10,8 @@
   <meta name="author" content="">
 
   <title>Aplikasi UPTD</title>
+  <link rel="icon" type="image/png" href="<?=base_url().'assets/'?>img/logo.png">
+
 
   <!-- Custom fonts for this template-->
   <link href="<?=base_url().'assets/'?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
