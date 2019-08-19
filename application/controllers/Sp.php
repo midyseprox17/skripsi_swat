2e35b4531316cdd7bba24af9abb6ae4cb21bc32a
midyseprox17 <midyseprox17@gmail.com>
2019-08-19 15:13:51 +0700

    sp

--- application/controllers/Sp.php
+++ application/controllers/Sp.php
@@ -17,9 +17,13 @@ class sp extends CI_Controller
 
 	public function tambah(){
 		if($this->session->userdata('masuk') == '1'){
-			$this->load->view('global/v_sidebar');
-			$this->load->view('sp/v_sp_tambah');
-			$this->load->view('global/v_footer');
+			if(isset($_POST['submit'])){
+
+			}else{
+				$this->load->view('global/v_sidebar');
+				$this->load->view('sp/v_sp_tambah');
+				$this->load->view('global/v_footer');
+			}
 		}else{
 			redirect(base_url().'login');
 		}
