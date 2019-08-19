e21e325c5da4ef72b5af1ce2dd4efc920179bf4b
Rizalu Ilman <ilmanrizalu@gmail.com>
2019-08-19 10:02:22 +0700

    v_content update

--- application/views/global/v_content.php
+++ application/views/global/v_content.php
@@ -1,3 +1,5 @@
 <center>
-  <img src="<?=base_url().'assets/'?>img/logo.png" width="50%">
+	<h1>UPTD Pengawasan Ketenagakerjaan</h1>
+	<h1>Wilayah IV Bandung</h1>
+  	<img class="mt-5" src="<?=base_url().'assets/'?>img/logo.png" width="30%">
 </center>
