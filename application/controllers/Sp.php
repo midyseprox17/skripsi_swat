5eef52adbfa8fdf62026b98d974283f6bdb9fec2
midyseprox17 <midyseprox17@gmail.com>
2019-08-19 15:34:54 +0700

    Update Sp.php

--- application/controllers/Sp.php
+++ application/controllers/Sp.php
@@ -18,6 +18,22 @@ class sp extends CI_Controller
 	public function tambah(){
 		if($this->session->userdata('masuk') == '1'){
 			if(isset($_POST['submit'])){
+				$status_tanggal = $_POST['status_tanggal'];
+				$tgl_d = "";
+				$tgl_m = "";
+				$tgl_y = "";
+
+				if($status_tanggal == "sekarang"){
+					$tgl_d = date('d');
+					$tgl_m = date('m');
+					$tgl_y = date('Y');
+				}else{
+					$tgl_d = $_POST['tgl_d'];
+					$tgl_m = $_POST['tgl_m'];
+					$tgl_y = $_POST['tgl_y'];
+				}
+
+				
 
 			}else{
 				$this->load->view('global/v_sidebar');
