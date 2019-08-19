1a9ac1993026480ed53bce1c4de7a27deccd9825
Rizalu Ilman <ilmanrizalu@gmail.com>
2019-08-19 23:13:27 +0700

    Update v_sp_tambah.php

--- application/views/sp/v_sp_tambah.php
+++ application/views/sp/v_sp_tambah.php
@@ -12,68 +12,79 @@
 				<div class="card-body">
 					<form>
 						<input type="hidden" name="<?=$this->security->get_csrf_token_name();?>" value="<?=$this->security->get_csrf_hash();?>" style="display: none">
-
-						<div class="form-group row">
-					    	<label class="col-sm-2 col-form-label" style="color: #000000">Jumlah No SP</label>
-					    <div class="col-sm-5">
-					      <select class="custom-select form-control form-control-sm">
-						    <option value="1">1</option>
-						    <option value="2">2</option>
-						    <option value="3">3</option>
-						  </select>
-					    </div>
-						</div>
-						<hr>
-					  	<div class="row">
-					      <legend class="col-form-label col-sm-2 pt-0" style="color: #000000">Tanggal Surat</legend>
-					      <div class="col-sm-10">
-					        <div class="form-check">
-					          <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
-					          <label class="form-check-label" for="gridRadios1">
-					            Tanggal Sekarang
-					          </label>
-					        </div>
-					        <div class="form-check">
-					          <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
-					          <label class="form-check-label" for="gridRadios2">
-					            Pilih tanggal
-					          </label>
-					        </div>
-					        <div class="form-group row mt-2">
-					        	<div class="col-sm-2">
-					        		<input type="text" class="form-control" placeholder="Tanggal">
-					        	</div>
-					        	<div class="col-sm-2">
-					        		<input type="text" class="form-control" placeholder="Bulan">
-					        	</div>
-					        	<div class="col-sm-2">
-					        		<input type="text" class="form-control" placeholder="Tahun">
-					        	</div>
-					        </div>
-					      </div>
-					  	</div>
-					  	<hr>
-					     <div class="form-group row">
-					    	<label class="col-sm-2 col-form-label" style="color: #000000">Nama</label>
-						    <div class="col-sm-5">
-						      <input type="text" class="form-control" placeholder="Nama">
-						    </div>
-						    <div class="col-sm-1">
-						      <button type="button" class="btn btn-primary" id="tambah_jenis" onclick="tambah_baris()"><i class="fa fa-plus"></i></button>
-						    </div>
-						</div>
-						<hr>
-						 <div class="tile-footer">
+						<table class="table">	
+							<tr>
+								<td style="width: 20%">Jumlah Nomor SP</td>
+								<td>
+									<div class="float-left" style="width: 30%">
+								      <select class="custom-select form-control form-control-sm">
+									    <option value="1">1</option>
+									    <option value="2">2</option>
+									    <option value="3">3</option>
+									  </select>
+								    </div>
+								</td>
+							</tr>
+							<tr>
+								<td>Nomor Surat</td>
+								<td><input class="form-control float-left" type="text" name="telp" style="width: 30%"></td>
+							</tr>
+							<tr>
+								<td>Tanggal Surat</td>
+								<td>
+									<div class="col-sm-10">
+								        <div class="form-check">
+								          <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+								          <label class="form-check-label" for="gridRadios1">
+								            Tanggal Sekarang
+								          </label>
+								        </div>
+								        <div class="form-check">
+								          <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+								          <label class="form-check-label" for="gridRadios2">
+								            Pilih tanggal
+								          </label>
+								        </div>
+								        <div class="form-group row mt-2">
+								        	<div class="col-sm-2">
+								        		<input type="text" class="form-control" placeholder="Tanggal">
+								        	</div>
+								        	<div class="col-sm-2">
+								        		<input type="text" class="form-control" placeholder="Bulan">
+								        	</div>
+								        	<div class="col-sm-2">
+								        		<input type="text" class="form-control" placeholder="Tahun">
+								        	</div>
+								        </div>
+								    </div>
+					    		</td>
+							</tr>
+						</table>
+						<table class="table" id="tbl_transaksi">
+							<tr id="tbl_transaksi1">
+								<td>Laundry</td>
+								<td><select class="form-control" name="id_jenis[]" id="id_jenis1" onchange="get_harga_jenis('1')">
+										<?php foreach ($jenis->result() as $j) {
+										?>
+											<option value="<?php echo $j->id; ?>"><?php echo $j->nama; ?></option>
+										<?php
+										}
+										?>
+									</select></td>
+								<td><input class="form-control" type="text" name="harga_jenis[]" id="harga_jenis1" readOnly></td>
+								<td><input class="form-control" type="number" name="qty[]"></td>
+								<td><button type="button" class="btn btn-success" id="tambah_jenis" onclick="tambah_baris()"><i class="fa fa-plus"></i></button></td>
+							</tr>
+						</table>
+						<div class="tile-footer">
 			            	<button class="btn btn-primary" type="submit" name="submit">Tambah Data</button>
-			             </div>
-
+			            </div>
 					</form>
 				</div>
 			</div>
 		</div>		
 	</div>
 </div>
-
 <script type="text/javascript">
 	function delete_row(rowno)
 	{
