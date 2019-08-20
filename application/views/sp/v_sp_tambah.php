b0d8eb6b2cebadc5a8a72bb0d4c08689b07dd8aa
midyseprox17 <midyseprox17@gmail.com>
2019-08-20 11:35:47 +0700

    Update v_sp_tambah.php

--- application/views/sp/v_sp_tambah.php
+++ application/views/sp/v_sp_tambah.php
@@ -1,7 +1,5 @@
 <head>
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
-    <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
 </head>
 
 <div class="container">
