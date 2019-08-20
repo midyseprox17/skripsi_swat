9436b8359880c9fcf826c3eee2bb8f92a30bab3e
Rizalu Ilman <ilmanrizalu@gmail.com>
2019-08-20 10:50:04 +0700

    DKMALKFNSA

--- application/views/sp/v_sp_tambah.php
+++ application/views/sp/v_sp_tambah.php
@@ -1,9 +1,7 @@
 <head>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
-    <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+	<script src="<?=base_url().'assets/'?>vendor/jquery/jquery.min.css"></script>
+    <script src="<?=base_url().'assets/'?>js/gijgo.js" type="text/javascript"></script>
+    <link href="<?=base_url().'assets/'?>css/gijgo.min.css" rel="stylesheet" type="text/css" />
 </head>
 
 <div class="container">
