f8c01cb957ebfc9190a7dac1e9788f479114c0de
Rizalu Ilman <ilmanrizalu@gmail.com>
2019-08-21 10:30:51 +0700

    Update v_sp.php

--- application/views/sp/v_sp.php
+++ application/views/sp/v_sp.php
@@ -4,11 +4,11 @@
 
 <div class="container">
 	<div class="row">
-		<div class="col-xl-10">
+		<div class="col-xl-3">
 		</div>
-		<div class="col-xl-2">
+		<div class="col-xl-9">
 			<a href="<?=base_url().'sp/tambah'?>" class="float-right d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm mb-3"><i class="fas fa-plus fa-sm text-white-50"></i> Tambah Data(Belum Bernomor)</a>
-			<a href="<?=base_url().'sp/tambah'?>" class="float-right d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm mb-3"><i class="fas fa-plus fa-sm text-white-50"></i> Tambah Data(Sudah Bernomor)</a>		
+			<a href="<?=base_url().'sp/tambah'?>" class="float-right d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm mb-3  mr-2"><i class="fas fa-plus fa-sm text-white-50"></i> Tambah Data(Sudah Bernomor)</a>		
 		</div>
 	</div>
 	<div class="row">
